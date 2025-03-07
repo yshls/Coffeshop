44228078f24d574b49f9d2db86f673b9a917a7a7
yshls <shluxnsal01@gmail.com>
2025-03-07 16:14:51 +0900

    로그인 완료

--- src/main/java/com/shop/cafe/dao/MemberDao.java
+++ src/main/java/com/shop/cafe/dao/MemberDao.java
@@ -3,6 +3,8 @@ package com.shop.cafe.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
@@ -23,6 +25,26 @@ public class MemberDao {
 
     @Value("${spring.datasource.password}")
     private String DB_PW;
+    
+    public Member login(Member m) throws Exception {
+        System.out.println("MemberDao login() 호출됨");
+
+        Class.forName(DB_DRIVER);
+        Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PW);
+
+        PreparedStatement stmt=con.prepareStatement("select * from member where email=? and pwd=? ");
+
+        stmt.setString(1, m.getEmail());
+        stmt.setString(2, m.getPwd());
+
+        ResultSet rs=stmt.executeQuery();
+        if(rs.next()){
+            String nickname=rs.getString("nickname");
+            m.setNickname(nickname);
+        }
+        return m;
+    }
+
 
     public void insertMember(Member m) throws Exception {
         System.out.println("MemberDao insertMember() 호출됨");
